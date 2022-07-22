326f8466e15ad79a2b7a2b25f4604087e36721ba
Fréderick Van der Haegen <f.vanderhaegen@codefined.be>
2022-07-22 16:25:14 +0200

    Add angular support

---
+++ src/parsers/angular.ts
@@ -0,0 +1,29 @@
+import prettierParserHTML from 'prettier/parser-html'
+import { CreateSortClassList, SortClassList } from '../sort-class-list'
+
+export default (cscl: CreateSortClassList) => ({
+	...prettierParserHTML.parsers.angular,
+	parse(text, parsers, options) {
+		const ast = prettierParserHTML.parsers.angular.parse(text, parsers, options)
+
+		const cleanElementClasses = el => {
+			if (el.attrs) {
+				const classAttr = el.attrs.find(attr => attr.name === 'class')
+				if (classAttr) {
+					const classList = classAttr.value
+						.split(' ')
+						.map(classItem => classItem.trim())
+						.filter(classItem => classItem.length > 0)
+					classAttr.value = cscl(options)(classList).join(' ')
+				}
+			}
+
+			if (el.children && el.children.length > 0) {
+				el.children.forEach(childEl => cleanElementClasses(childEl))
+			}
+		}
+		cleanElementClasses(ast)
+
+		return ast
+	},
+})
--- src/parsers/index.ts
+++ src/parsers/index.ts
@@ -1,10 +1,12 @@
 import { CreateSortClassList, SortClassList } from '../sort-class-list'
+import angular from './angular'
 import babel from './babel'
 import css from './css'
 import html from './html'
 import typescript from './typescript'
 
 export default (cscl: CreateSortClassList) => ({
+	angular: angular(cscl),
 	html: html(cscl),
 	css: css(cscl),
 	babel: babel(cscl),
